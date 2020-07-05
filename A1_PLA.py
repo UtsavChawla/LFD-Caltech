99556d27cdf7bfd3c20ae0ff90adac32387881f2
UtsavChawla <utsav24chawla@gmail.com>
2020-07-05 21:28:30 +0530

    Perceptron - efficient version

--- A1_PLA.py
+++ A1_PLA.py
@@ -1,91 +1,81 @@
 import matplotlib.pyplot as plt
 import random as rd
 import numpy as np
+import pandas as pd
 
-# Defining a function to check convergence
-def convergence(p0, p1, p2, setpos, setneg, xi, yi):
-    for i in setpos:
-        if ((p0) + (p1*xi[i]) + (p2*yi[i])) <= 0:
-            return False
-    for i in setneg:
-        if ((p0) + (p1*xi[i]) + (p2*yi[i])) > 0:
-            return False
-    return True
+def datacreation(N):
+    # Data generation
+    data = pd.DataFrame( (np.random.rand(N, 2)*2) - 1, columns=['ix1','ix2'])
+    data['ix0'] = 1
+    cols = data.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    data = data[cols]
 
-def perceptron():
-    # Step 1 : Creating input space
-    xi = list()
-    yi = list()
-    for i in range(10):
-        xi.append(rd.uniform(-1, 1))
-        yi.append(rd.uniform(-1, 1))
-
-    # Step 2 : Defining function f (y+ax+b = 0)
+    # Defining function f (iw0*ix0 + iw1*ix1 + iw2*ix2 = 0)
     xf = [rd.uniform(-1, 1), rd.uniform(-1, 1)]
     yf = [rd.uniform(-1, 1), rd.uniform(-1, 1)]
     coefficients = np.polyfit(xf, yf, 1)
-    a= -coefficients[0]
-    b= -coefficients[1]
+    iw = np.zeros(shape=3)
+    iw[2] = 1
+    iw[1] = -coefficients[0]
+    iw[0] = -coefficients[1]
 
-    # Step 3 : classifying values according to f
-    setpos = list()
-    setneg = list()
-    for i in range(10):
-        if (yi[i] + (a*xi[i]) + b) <= 0 :
-            setneg.append(i)
-        else:
-            setpos.append(i)
+    # Assigning y values
+    data['iy'] = -1
+    data.loc[(iw[0]*data['ix0'] + iw[1]*data['ix1'] + iw[2]*data['ix2']) >= 0, 'iy'] = 1
 
-    # Step 4 : Finding function g (w0+w1x+w2y = 0) using PLA
-    w0 = 0
-    w1 = 0
-    w2 = 0
+    return data, iw
 
+def pla(data):
+    #Converting dataframe to matrix
+    features = np.asarray(data.loc[:, 'ix0':'ix2'])
+
+    # set weights to zero
+    w = np.zeros(shape=(1, features.shape[1]))
+
+    # Iterating till convergence
     num = 0
-    while(convergence(w0, w1, w2, setpos, setneg, xi, yi ) == False):
-        index = rd.randint(0, 9)
+    data['g'] = 5
+    missclassified = data[(data['g'] != data['iy'])]
+    while(len(missclassified) > 0):
+        data['graw'] = np.dot(features, w.transpose())
+        data['g'] = -1
+        data.loc[data['graw'] >= 0, 'g'] = 1
+        missclassified = data[(data['g'] != data['iy'])]
+        if(len(missclassified) == 0):
+            break
         num = num + 1
-        if (index in setpos) and ((w0 + (w1*xi[index]) + (w2*yi[index])) <= 0) :
-            w0 = w0 + 1
-            w1 = w1 + xi[index]
-            w2 = w2 + yi[index]
-            continue
-        if (index in setneg) and ((w0 + (w1 * xi[index]) + (w2 * yi[index])) > 0):
-            w0 = w0 - 1
-            w1 = w1 - xi[index]
-            w2 = w2 - yi[index]
-            continue
-        continue
+        index = rd.randint(0, len(missclassified) - 1)
+        if(missclassified.iloc[index]['g'] == 1):
+            w = w - features[index]
+        else:
+            w = w + features[index]
 
-    return num
+    return w,num
 
-rns = 0
-for rnup in range(1000):
-    print(rnup)
-    rns = rns + perceptron()
+def plotcurve(data, wieght):
+    #Converting wieght into right coefficient
+    coefficients = np.zeros(shape=2)
+    coefficients[0] = -wieght[0,1]/wieght[0,2]
+    coefficients[1] = -wieght[0,0]/wieght[0,2]
 
-
-
-
-
-
-# Appendix 1: Plotting input function f
-polynomial = np.poly1d(coefficients)
-x_axis = np.linspace(-1,1,100)
-y_axis = polynomial(x_axis)
-plt.plot(x_axis, y_axis)
-for i in range(10):
-    if i in setpos:
-        plt.plot(xi[i], yi[i], 'go')
-    else:
-        plt.plot(xi[i], yi[i], 'ro')
-
-coeg = coefficients.copy()
-coeg[0] = -1 * (w1/w2)
-coeg[1] = -1 * (w0/w2)
-polynomial = np.poly1d(coeg)
-x_axis = np.linspace(-1,1,100)
-y_axis = polynomial(x_axis)
-plt.plot(x_axis, y_axis)
+    #Plotting equation and data
+    polynomial = np.poly1d(coefficients)
+    x_axis = np.linspace(-1,1,100)
+    y_axis = polynomial(x_axis)
+    plt.plot(x_axis, y_axis)
+    plt.scatter(np.array(data[data['iy']==1]['ix1']), np.array(data[data['iy']==1]['ix2']), marker='o', label='1')
+    plt.scatter(np.array(data[data['iy']==-1]['ix1']), np.array(data[data['iy']==-1]['ix2']), marker='x', label='-1')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.legend()
+    plt.show()
 
 
+#Finally Executing
+data = datacreation(10)[0]
+output = pla(data)
+wieghts = output[0]
+num = output[1]
+
+plotcurve(data, wieghts)
