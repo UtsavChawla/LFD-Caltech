f9683ce868886d4fc1da6716742f32fa837202b8
UtsavChawla <utsav24chawla@gmail.com>
2020-08-08 16:29:29 +0530

    Adding coordinate Descent for comparision

--- A5_GradientDescent.py
+++ A5_GradientDescent.py
@@ -8,14 +8,32 @@ u = dc.Decimal(1)
 v = dc.Decimal(1)
 runs = 0
 E = (u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u)) ** dc.Decimal(2)
+# Two choices for methods - (a) Gradient Descent and (b) Coordinate Descent
+method = 'b'
 
-## Iterating to meet the given conditiion
-while (E > (10 ** -14)):
-    uDer = dc.Decimal(2) * (dc.Decimal.exp(v) + dc.Decimal(2) * v * dc.Decimal.exp(-u)) * (u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u))
-    vDer = dc.Decimal(2) * (dc.Decimal(u) * dc.Decimal.exp(v) - dc.Decimal(2) * dc.Decimal.exp(-u)) * (u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u))
-    u = u - dc.Decimal(n * uDer)
-    v = v - dc.Decimal(n * vDer)
-    E = (u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u)) ** dc.Decimal(2)
-    runs = runs + 1
-    print(runs)
-    print(E*(10**14))
+## Gradient Descent
+if (method == 'a'):
+    while (E > (10 ** -14)):
+        uDer = dc.Decimal(2) * (dc.Decimal.exp(v) + dc.Decimal(2) * v * dc.Decimal.exp(-u)) * (
+                    u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u))
+        vDer = dc.Decimal(2) * (dc.Decimal(u) * dc.Decimal.exp(v) - dc.Decimal(2) * dc.Decimal.exp(-u)) * (
+                    u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u))
+        u = u - dc.Decimal(n * uDer)
+        v = v - dc.Decimal(n * vDer)
+        E = (u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u)) ** dc.Decimal(2)
+        runs = runs + 1
+        print(runs)
+        print(E)
+
+## Coorodiante Descent
+if (method == 'b'):
+    for runs in range(15):
+        uDer = dc.Decimal(2) * (dc.Decimal.exp(v) + dc.Decimal(2) * v * dc.Decimal.exp(-u)) * (
+                    u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u))
+        u = u - dc.Decimal(n * uDer)
+        vDer = dc.Decimal(2) * (dc.Decimal(u) * dc.Decimal.exp(v) - dc.Decimal(2) * dc.Decimal.exp(-u)) * (
+                    u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u))
+        v = v - dc.Decimal(n * vDer)
+        E = (u * dc.Decimal.exp(v) - dc.Decimal(2) * v * dc.Decimal.exp(-u)) ** dc.Decimal(2)
+        print(runs + 1)
+        print(E)
