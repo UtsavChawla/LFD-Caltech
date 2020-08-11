9f3c2e73e0235b231c57db149effc4106a10a0b5
UtsavChawla <utsav24chawla@gmail.com>
2020-08-11 23:48:21 +0530

    Logistic Regression Algo using Schotastic Gradient Descent

--- A5_LogisticRegression.py
+++ A5_LogisticRegression.py
@@ -2,6 +2,7 @@ import numpy as np
 import random as rd
 import pandas as pd
 import matplotlib.pyplot as plt
+import math as mt
 
 
 # Defining function f(x) = if(iw0*ix0 + iw1*ix1 + iw2*ix2>0) then 1 else 0 ; returns weight
@@ -24,7 +25,7 @@ def labeleddata(N, w):
     cols = data.columns.tolist()
     cols = cols[-1:] + cols[:-1]
     data = data[cols]
-    data['iy'] = 0
+    data['iy'] = -1
     data.loc[(w[0] * data['ix0'] + w[1] * data['ix1'] + w[2] * data['ix2']) > 0, 'iy'] = 1
     return data
 
@@ -46,8 +47,60 @@ def plotlinearcurve(data, wieght):
     plt.show()
 
 
+# Logistic regression to predict outcome
+def logisticregression(data):
+    ow = np.zeros(3)
+    eta = 0.01
+
+    delta = 1
+    runs = 0
+
+    while (delta > 0.01):
+
+        if(runs % 100 == 0):
+            print(runs)
+
+        ow_old = ow
+        arr = np.array(range(100))
+        np.random.shuffle(arr)
+        for p in arr:
+            xn = np.array(data.iloc[p][0:3])
+            yn = np.array(data.iloc[p][3:4])
+            DerEn = (-1) * (yn * xn) / (1 + np.exp(yn * np.dot(ow.T, xn)))
+            ow = ow - (eta * DerEn)
+
+        delta = np.linalg.norm(ow - ow_old)
+        runs = runs + 1
+
+    return ow, runs
+
+
 # Execution
+execs = 10
 N = 100
-w = inputfunc()
-data = labeleddata(N, w)
-plotlinearcurve(data, w)
+runs = 0
+
+for i in range(execs):
+    print(i)
+    iw = inputfunc()
+    data = labeleddata(N, iw)
+    output = logisticregression(data)
+    ow = output[0]
+    runs = runs + output[1]
+
+runs = runs/execs
+
+
+
+# calculating cross entropy error
+#eN = 1000
+#edata = labeleddata(1000, iw)
+#edata['Error'] = 0.1
+
+#for p in range(len(edata)):
+ #   xn = np.array(edata.iloc[p][0:3])
+  #  yn = np.array(edata.iloc[p][3:4])
+   # E = np.log(1 + np.exp(-yn * np.dot(ow.T, xn))).mean()
+    #edata.at[p,'Error'] = E
+
+#error = edata['Error'].mean()
