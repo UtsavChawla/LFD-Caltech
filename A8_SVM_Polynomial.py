a6f7ffbf8f6e2ebd08089e205697577fe6163ee3
UtsavChawla <utsav24chawla@gmail.com>
2020-08-30 05:58:28 +0530

    SVM - checking effect of varying C

--- A8_SVM_Polynomial.py
+++ A8_SVM_Polynomial.py
@@ -12,7 +12,7 @@ def fetchdata():
         f_out.write(request_out.text)
 
 
-def labeldata(Y, k):
+def labeldata_1vsall(Y, k):
     Yn = Y.copy()
     Yn[Yn != k] = -1
     Yn[Yn == k] = 1
@@ -26,23 +26,33 @@ def errorcompute(model, Xtrain, Ytrain):
 
 
 ##Execution
-# Preparing Data
+# Preparing Data and labeling for 1 vs 5 classifier
 # fetchdata()
 data_train = np.loadtxt("train.dta")
 data_test = np.loadtxt("test.dta")
+
+tp1 = data_train[data_train[:,0]==1]
+tp2 = data_train[data_train[:,0]==5]
+tp2[:,0] = -1
+data_train = np.append(tp1, tp2, axis=0)
+
+tp1 = data_test[data_test[:,0]==1]
+tp2 = data_test[data_test[:,0]==5]
+tp2[:,0] = -1
+data_test = np.append(tp1, tp2, axis=0)
+del tp1, tp2
+
 Xtrain = data_train[:, 1:3]
-Ytrain_base = data_train[:, 0]
+Ytrain = data_train[:, 0]
 Xtest = data_test[:, 1:3]
-Ytest_base = data_test[:, 0]
+Ytest = data_test[:, 0]
 del data_train, data_test
 
-for k in range(10):
-    # Labeling Data
-    Ytrain = labeldata(Ytrain_base, k)
-    Ytest = labeldata(Ytest_base, k)
-
-    # Modeling
-    model = svm.SVC(C=0.01, degree=2, kernel='poly', coef0=1, gamma=1)
+# Modeling
+for C in [0.001, 0.01, 0.1, 1]:
+    model = svm.SVC(C=C, degree=2, kernel='poly', coef0=1, gamma=1)
     model.fit(Xtrain, Ytrain)
-    error = errorcompute(model, Xtrain, Ytrain)
-    print(k, error, len(model.support_vectors_))
+    num_sv = len(model.support_vectors_)
+    e_in = errorcompute(model, Xtrain, Ytrain)
+    e_out = errorcompute(model, Xtest, Ytest)
+    print(C, e_in, e_out, num_sv)
